63e2a97faf93b0d9be3d233de22e2a48d1975eab
Logar <simon.boeuf@gmail.com>
2013-05-28 22:07:13 +0200

    Added a start of videolist ajax management

--- webroot/Scripts/scripts.js
+++ webroot/Scripts/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){    
-    var json = "[{\"imageUrl\":\"dataimageUrl\",\"soundUrl\":\"datassound\"},{\"imageUrl\":\"datasimage2\",\"soundUrl\":\"datassound2\"}]";
+    /*
+    var json = "[{\"imageUrl\":\"lemonde.fr\",\"soundUrl\":\"korben.info\"},{\"imageUrl\":\"google.fr\",\"soundUrl\":\"youtube.com\"}]";
+    //var json=getVideos(1,3);
     var videosList = jQuery.parseJSON(json);
     for (var i = 0; i < videosList.length; i++) {
         var object = videosList[i];
@@ -7,4 +9,23 @@ $(document).ready(function(){
         var videoID = object.imageUrl; 
         alert("video : " + videoID + " sound : " + soundID);
     }
+    */
+
+   getVideos(1,2);
+   function getVideos(page, numberOfResult)
+   {
+       var url = 'http://127.0.0.1:8182/videoresources/'+page+'/'+numberOfResult;
+       $.get(url, function(json) {
+        videosList = jQuery.parseJSON(json);
+        if(videosList !== null){
+             for (var i = 0; i < videosList.length; i++) {
+                 var object = videosList[i];
+                 var soundID = object.soundUrl;
+                 var videoID = object.imageUrl; 
+                 alert("video : " + videoID + " sound : " + soundID);
+             }
+         }
+        else console.log("Bad json");
+        });
+   }
 });
