5d5d7149ce1d97e1813c8ca4b78aa73064f52ec9
Logar <simon.boeuf@gmail.com>
2013-05-28 18:13:50 +0200

    Modified client json parsing to avoid using eval

--- webroot/Scripts/scripts.js
+++ webroot/Scripts/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){    
     var json = "[{\"imageUrl\":\"dataimageUrl\",\"soundUrl\":\"datassound\"},{\"imageUrl\":\"datasimage2\",\"soundUrl\":\"datassound2\"}]";
-    var videosList = eval(json);
-
+    var videosList = jQuery.parseJSON(json);
     for (var i = 0; i < videosList.length; i++) {
         var object = videosList[i];
         var soundID = object.soundUrl;
