1e7a89d0abf9f895be4341f875e75fff47905191
Mike <albrecht.michael@hotmail.fr>
2013-06-01 23:52:31 +0200

    improved design

--- webroot/Scripts/scripts.js
+++ webroot/Scripts/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
 function getVideos(page, numberOfResult) {
     var url = '/videoresources/' + page + '/' + numberOfResult;
+
     $.getJSON(url, function (videosList) {
         if (videosList != null) {
             createVideosList(videosList)
@@ -17,11 +18,12 @@ function createVideosList(videosList) {
         var title = value.title;
         var soundID = value.soundUrl;
         var videoID = value.imageUrl;
-        $("#videosList").append("<div><p>"+title+"</p><a onclick=\"playVideos('"+soundID+"','"+videoID+"');\" value=\""+title+"\"><img class=\"miniature\" src=\"//i2.ytimg.com//vi/"+videoID+"/mqdefault.jpg\" width=\"90%\" /></a></div>");
+        $("#videosList").append("<div><p>"+title+"</p><a href=\"#\" onclick=\"playVideos('"+title+"','"+soundID+"','"+videoID+"');\" value=\""+title+"\"><img class=\"miniature\" src=\"//i2.ytimg.com//vi/"+videoID+"/mqdefault.jpg\" width=\"90%\" /></a></div>");
     });
 }
 
-function playVideos(soundID, videoID) {
+function playVideos(title, soundID, videoID) {
+    $("#title").text(title);
     $("#soundplayer").attr("src", getEmbedUrl(soundID));
     $("#vidplayer").attr("src", getEmbedUrl(videoID));
 }
