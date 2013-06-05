b628374d8c788df5b4767c609b7f23c7d21bb9ef
Logar <simon.boeuf@gmail.com>
2013-06-05 11:11:18 +0200

    mad a custom object for videos in jquery

--- webroot/Scripts/videos.js
+++ webroot/Scripts/videos.js
@@ -1,10 +1,46 @@
-//Description : Loads a list of videos
+//Description : Video Class
+function cVideo( title, video, sound ){
+	this.title = title;
+	this.video = video;
+        this.sound = sound;
+
+	$( this ).bind( "getTitle", function(){
+		return this.title;
+	});
+        
+        $( this ).bind( "getVideo", function(){
+		return this.video;
+	});
+        
+        $( this ).bind( "getSound", function(){
+        return this.sound;
+	});
+}
+
+////Description : Loads a list of videos
 //args
 //page : number of the page to reach
 //numberOfResult : number of videos to get
 function feedVideosList(page, numberOfResult) {
 
     var url = '/videoresources/' + page + '/' + numberOfResult;
+
+    $.getJSON(url, function(videosList) {
+        if (videosList !== null) {
+            $.each(videosList, function(key, value) {
+                var title = value.title;
+                var soundID = value.soundUrl;
+                var videoID = value.imageUrl;
+                $("#videosList").append("<p>" + title + "</p><a href=\"#\" onclick=\"playVideos('" + title + "','" + soundID + "','" + videoID + "');\" value=\"" + title + "\"><img class=\"miniature\" src=\"//i2.ytimg.com//vi/" + videoID + "/mqdefault.jpg\" width=\"90%\" /></a>");
+            });
+        }
+        else
+            console.log("Bad json");
+    });
+}
+
+function getFirstVideo() {
+    var url = '/videoresources/1/1';
 
     $.getJSON(url, function(videosList) {
         if (videosList !== null) {
